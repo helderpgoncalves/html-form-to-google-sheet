3e761fa81f52cd1a3c4d44dbdcc1af30f37afcf3
Hélder Gonçalves <helderpimentaxc@gmail.com>
2022-11-07 20:07:30 +0000

    update

--- Code.js
+++ Code.js
@@ -6,6 +6,29 @@ function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
+function checkLogin(username, password) {
+  // extension | username | password | int_id
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var ws = ss.getSheetByName("credentials");
+  const dataRange = ws.getRange("A1").getDataRegion();
+  const data = dataRange.getDisplayValues();
+
+  var user = [];
+
+  // check if username and password match
+  for (var i = 1; i < data.length; i++) {
+    console.log(data[i][1], data[i][2]);
+    if (data[i][1] == username && data[i][2] == password) {
+      user.push({
+        extension: data[i][0],
+        int_id: data[i][3],
+      });
+    }
+  }
+
+  return user;
+}
+
 function processForm(formObject) {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var ws = ss.getSheetByName("database");
@@ -44,6 +67,44 @@ function processForm(formObject) {
   }
 }
 
+function processFormRouter(formObject) {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var ws = ss.getSheetByName("routers_db");
+  const dataRange = ws.getRange("A1").getDataRegion();
+  const data = dataRange.getDisplayValues();
+
+  if (data.length > 1) {
+    var lastRow = data.pop();
+    var lastId = parseInt(lastRow[0]);
+    var newId = lastId + 1;
+  } else {
+    var newId = 1500000;
+  }
+
+  var currentDate = new Date().toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles",
+  });
+
+  try {
+    ws.appendRow([
+      newId,
+      formObject.access_code_router,
+      formObject.first_name_router || "",
+      formObject.last_name_router || "",
+      formObject.employee_id_router || "",
+      formObject.call_center_id_router || "",
+      formObject.language_router || "",
+      formObject.int_id_router || "",
+      formObject.start_router || "",
+      currentDate,
+      formObject.extension_router || "",
+      formObject.init_timestamp_router || "",
+    ]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 // Get Access Codes (B Column) and H Column
 function getAccessCodesAndIntIds() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
@@ -58,8 +119,6 @@ function getAccessCodesAndIntIds() {
       int_id: data[i][7],
     });
   }
-
-  console.log(accessCodesAndIntIds);
 
   return accessCodesAndIntIds;
 }
@@ -96,8 +155,6 @@ function getInterpreters(language) {
       });
     }
   }
-
-  console.log("cuidosiahasdsa:" + language);
 
   return interpreters;
 }
